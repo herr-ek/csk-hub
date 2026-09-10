615627a27e2cc53c333c1f2394da88a3f35caaf2
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2026-09-10 07:51:09 +0200

    fix: correct conditional

--- src/core/db/index.ts
+++ src/core/db/index.ts
@@ -4,6 +4,8 @@ import { env } from "@/core/config/env"
 const databaseUrl = new URL(env.POSTGRES_URL)
 
 // TODO: Remove in prod!
-databaseUrl.searchParams.set("sslmode", "no-verify")
+if (databaseUrl.searchParams.has("sslmode")) {
+  databaseUrl.searchParams.set("sslmode", "no-verify")
+}
 
 export const db = drizzle(databaseUrl.toString())
